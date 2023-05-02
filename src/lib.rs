b74ce0f8c8378dbefd1636ffee60173ae8dd6d05
Pranjal-Patel <pranjalpatel9838@gmail.com>
2023-05-02 15:41:45 +0530

    improved to_lowercase and to_uppercase functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,26 @@
-const MASK: u8 = 1 << 5;
-
+#[inline]
 pub fn is_uppercase(c: char) -> bool {
-    c as u8 & MASK == 0
+    c as u8 & 1 << 5 == 0
 }
 
+#[inline]
 pub fn is_lowercase(c: char) -> bool {
-    c as u8 & MASK != 0
+    c as u8 & 1 << 5 != 0
 }
 
+#[inline]
 pub fn toggle_case(c: char) -> char {
-    (c as u8 ^ MASK) as _
+    (c as u8 ^ 1 << 5) as _
 }
 
+#[inline]
 pub fn to_uppercase(c: char) -> char {
-    if is_uppercase(c) {
-        c
-    } else {
-        toggle_case(c)
-    }
+    (c as u8 ^ (is_lowercase(c) as u8 * (1 << 5))) as _
 }
 
+#[inline]
 pub fn to_lowercase(c: char) -> char {
-    if is_lowercase(c) {
-        c
-    } else {
-        toggle_case(c)
-    }
+    (c as u8 | (is_uppercase(c) as u8 * (1 << 5))) as _
 }
 
 #[cfg(test)]
